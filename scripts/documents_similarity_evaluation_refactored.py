54e9d431cb48f73452a41d43ca9c7baaa9fcf700
Pierre-Olivier Bonin, Ph.D. <31133836+pierreolivierbonin@users.noreply.github.com>
2025-07-11 07:52:33 -0400

    small improvements

--- scripts/documents_similarity_evaluation_refactored.py
+++ scripts/documents_similarity_evaluation_refactored.py
@@ -23,8 +23,9 @@ class RAGcorpusConsistencyEvaluator:
                  collection_name:str, 
                  similarity_fn_name:str,
                  ef_construction:int,
-                 threshold:int=50,
+                 top_n:int=50,
                  random_sample=False,
+                 random_n=100,
                  seed=123
                  ):
         
@@ -33,8 +34,9 @@ class RAGcorpusConsistencyEvaluator:
         self.collection_name = collection_name
         self.similarity_fn_name = similarity_fn_name
         self.ef_construction = ef_construction
-        self.threshold = threshold
+        self.top_n = top_n
         self.random_sample = random_sample
+        self.random_n = random_n
         self.seed = seed
         
     def _initialize_client(self):
@@ -64,7 +66,7 @@ class RAGcorpusConsistencyEvaluator:
         self._retrieve_all_docs()
         if self.random_sample:
             random.seed(1837)
-            rand_ix = random.choices(range(self.collection.count()), k=100)
+            rand_ix = random.choices(range(self.collection.count()), k=self.random_n)
             self.rand_sample = itemgetter(*rand_ix)(self.all_docs)
             self.all_docs = self.rand_sample
 
@@ -72,12 +74,12 @@ class RAGcorpusConsistencyEvaluator:
         ip_distances_with_identical_first_match = []
         dist_description = []
         if save_to_disk:
-            with open("./embedding_cosine_distance_distributions.txt", "w") as f:
+            with open(f"./embedding_cosine_distance_distributions_random{self.random_sample}_top{self.top_n}.txt", "w") as f:
                 for i in self.all_docs: # switch rand_sample to all_docs when runnign full experiment
 
                     # search the vectorDB with one of its chunks
                     results = self.collection.query(query_texts=i, 
-                                                    n_results=self.threshold, 
+                                                    n_results=self.top_n, 
                                                     include=["documents", "distances"])
                     print(f"\nDocument chunk cosine distances: {[format(d, '.2f') for d in results["distances"][0]]}")
                     f.writelines(str([format(d, '.2f') for d in results["distances"][0]])+"\n")
@@ -93,7 +95,7 @@ class RAGcorpusConsistencyEvaluator:
 
                         # search the vectorDB with one of its chunks
                         results = self.collection.query(query_texts=i, 
-                                                        n_results=self.threshold, 
+                                                        n_results=self.top_n, 
                                                         include=["documents", "distances"])
                         print(f"\nDocument chunk cosine distances: {[format(d, '.2f') for d in results["distances"][0]]}")
                         f.writelines(str([format(d, '.2f') for d in results["distances"][0]])+"\n")
@@ -109,7 +111,7 @@ class RAGcorpusConsistencyEvaluator:
             for d in dist_description:
                 print(f"\n{d}")
                     
-            with open(f"./documents_similarity_stats_first_{self.threshold}.csv", "w") as file:
+            with open(f"./documents_similarity_stats_top{self.top_n}.csv", "w") as file:
                 for d in dist_description:
                     file.writelines(str(d)+"\n")
 
@@ -126,8 +128,9 @@ if __name__ == "__main__":
                                              collection_name="labour_baseline",
                                              similarity_fn_name="cosine",
                                              ef_construction=1000,
-                                             threshold=25,
+                                             top_n=10,
                                              random_sample=True,
+                                             random_n=25,
                                              seed=1837
                                              )
     evaluator.initialize_docs_collection()
