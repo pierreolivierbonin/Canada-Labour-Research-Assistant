91df54bf853435c57509fe38c20d24a8fa96e8d0
Pierre-Olivier Bonin, Ph.D. <31133836+pierreolivierbonin@users.noreply.github.com>
2025-07-11 09:53:34 -0400

    improvements + manual validation loop

--- scripts/documents_similarity_evaluation_refactored.py
+++ scripts/documents_similarity_evaluation_refactored.py
@@ -7,15 +7,15 @@ from operator import itemgetter
 import random
 import sys
 sys.path.append("./")
+import warnings
 
 import chromadb
 from chromadb.config import Settings
 from scipy import stats
 
-from config import ChromaDBSettings
 from db_config import EmbeddingModel, ModelsConfig
 
-class RAGcorpusConsistencyEvaluator:
+class RAGcorpusSelfConsistencyEvaluator:
 
     def __init__(self, 
                  client_path:str, 
@@ -26,7 +26,8 @@ class RAGcorpusConsistencyEvaluator:
                  top_n:int=50,
                  random_sample=False,
                  random_n=100,
-                 seed=123
+                 seed=123,
+                 verbose=True
                  ):
         
         self.client_path = client_path
@@ -38,6 +39,7 @@ class RAGcorpusConsistencyEvaluator:
         self.random_sample = random_sample
         self.random_n = random_n
         self.seed = seed
+        self.verbose = verbose
         
     def _initialize_client(self):
         try:
@@ -61,7 +63,7 @@ class RAGcorpusConsistencyEvaluator:
                                                         configuration={"hnsw": {"space": self.similarity_fn_name,     # https://docs.trychroma.com/docs/collections/configure#spann-index-configuration
                                                                         "ef_construction": self.ef_construction}})
 
-    def find_similarity_score(self, save_to_disk=False):
+    def find_self_consistency_scores(self, save_to_disk=False):
 
         self._retrieve_all_docs()
         if self.random_sample:
@@ -75,39 +77,46 @@ class RAGcorpusConsistencyEvaluator:
         dist_description = []
         if save_to_disk:
             with open(f"./embedding_cosine_distance_distributions_random{self.random_sample}_top{self.top_n}.txt", "w") as f:
-                for i in self.all_docs: # switch rand_sample to all_docs when runnign full experiment
+                self.results_compiled = []
+                for ix, i in enumerate(self.all_docs): # switch rand_sample to all_docs when running full experiment
 
                     # search the vectorDB with one of its chunks
                     results = self.collection.query(query_texts=i, 
                                                     n_results=self.top_n, 
                                                     include=["documents", "distances"])
-                    print(f"\nDocument chunk cosine distances: {[format(d, '.2f') for d in results["distances"][0]]}")
+                    if self.verbose:
+                        print(f"\nDocument chunk cosine distances for top-{self.top_n} matches: {[format(d, '.2f') for d in results["distances"][0]]}")
                     f.writelines(str([format(d, '.2f') for d in results["distances"][0]])+"\n")
 
-                    if i!=results["documents"][0][0]: # that's where we validate that a candidate chunk and the first match are identical
+                    if i!=results["documents"][0][0]: # validate that a candidate chunk and the first match are identical
                         print(f"\nInconsistent search detected - Document chunk matched with non-identical chunk\nOriginal doc: {i[:100]}\nRetrieved doc: {results["documents"][0][0][:100]}")
                         non_identical_matches+=1
                     else:
                         ip_distances_with_identical_first_match.append(results["distances"])
                         dist_description.append(stats.describe(results["distances"][0]))
+                    self.results_compiled.append((i, results))
+            
         else:
-            for i in self.all_docs: # switch rand_sample to all_docs when runnign full experiment
+            self.results_compiled = []
+            for ix, i in enumerate(self.all_docs): # switch rand_sample to all_docs when runnign full experiment
 
-                        # search the vectorDB with one of its chunks
-                        results = self.collection.query(query_texts=i, 
-                                                        n_results=self.top_n, 
-                                                        include=["documents", "distances"])
-                        print(f"\nDocument chunk cosine distances: {[format(d, '.2f') for d in results["distances"][0]]}")
-                        f.writelines(str([format(d, '.2f') for d in results["distances"][0]])+"\n")
+                # search the vectorDB with one of its chunks
+                results = self.collection.query(query_texts=i, 
+                                                n_results=self.top_n, 
+                                                include=["documents", "distances"])
+                if self.verbose:
+                    print(f"\nDocument chunk cosine distances: {[format(d, '.2f') for d in results["distances"][0]]}")
+                f.writelines(str([format(d, '.2f') for d in results["distances"][0]])+"\n")
 
-                        if i!=results["documents"][0][0]: # that's where we validate that a candidate chunk and the first match are identical
-                            print(f"\nInconsistent search detected - Document chunk matched with non-identical chunk\nOriginal doc: {i[:100]}\nRetrieved doc: {results["documents"][0][0][:100]}")
-                            non_identical_matches+=1
-                        else:
-                            ip_distances_with_identical_first_match.append(results["distances"])
-                            dist_description.append(stats.describe(results["distances"][0]))
+                if i!=results["documents"][0][0]: # that's where we validate that a candidate chunk and the first match are identical
+                    warnings.warn(f"\nInconsistent search detected - Document chunk matched with non-identical chunk\nOriginal doc: {i[:100]}\nRetrieved doc: {results["documents"][0][0][:100]}")
+                    non_identical_matches+=1
+                else:
+                    ip_distances_with_identical_first_match.append(results["distances"])
+                    dist_description.append(stats.describe(results["distances"][0]))
+                self.results_compiled.append((i, results))
 
-        if save_to_disk:
+        if self.verbose:
             for d in dist_description:
                 print(f"\n{d}")
                     
@@ -115,15 +124,18 @@ class RAGcorpusConsistencyEvaluator:
                 for d in dist_description:
                     file.writelines(str(d)+"\n")
 
-        if self.random_sample:
-            print(f"\n\nNon-identical document chunks matched for the random sample of 100 documents in collection: {non_identical_matches}")
+        if self.random_sample and non_identical_matches>0:
+            warnings.warn(f"\n\nNon-identical document chunks matched for the random sample of 100 documents in collection: {non_identical_matches}")
+
+        return self.results_compiled
+
 
 if __name__ == "__main__":
 
     from db_config import EmbeddingModel, ModelsConfig
 
     selected_model = EmbeddingModel(model_name=ModelsConfig.models["mpnet"], trust_remote_code=True)
-    evaluator = RAGcorpusConsistencyEvaluator(client_path="./chroma_vectorDB_comparison",
+    evaluator = RAGcorpusSelfConsistencyEvaluator(client_path="./chroma_vectorDB_comparison",
                                              embedding_model_fn=selected_model.model_chroma_callable,
                                              collection_name="labour_baseline",
                                              similarity_fn_name="cosine",
@@ -134,4 +146,14 @@ if __name__ == "__main__":
                                              seed=1837
                                              )
     evaluator.initialize_docs_collection()
-    evaluator.find_similarity_score(save_to_disk=True)
+    results = evaluator.find_self_consistency_scores(save_to_disk=True)
+    print(len(results))
+
+    # manual validation that the queried documents resemble the retrieved documents
+    for ix, result in enumerate(results):
+        print(f"\nDocument queried... \n\n{result[0]}")
+        print(f"\nPreviewing top-{evaluator.top_n} matches...")
+        
+        for jx, j in enumerate(range(len(result[1]["documents"][0]))):
+            print(f"\nViewing matched Document-chunk rank #{jx+1}... \n...for Document-chunk query #{ix+1}...")
+            print(f"\n{result[1]["documents"][0][j]}")
